c7bf7733fb28511d80e991e5ceb7ba3d3b4f3402
JP Mudugal <jayaprakashmudugal@outlook.com>
2020-06-28 11:47:37 +0800

    adding spark3 code

---
+++ src/ratings-counter.py
@@ -0,0 +1,15 @@
+// Code for testing GIT
+//This code is in new source directory
+from pyspark import SparkConf, SparkContext
+import collections
+
+conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+sc = SparkContext(conf = conf)
+
+lines = sc.textFile("file:///SparkCourse/ml-100k/u.data")
+ratings = lines.map(lambda x: x.split()[2])
+result = ratings.countByValue()
+
+sortedResults = collections.OrderedDict(sorted(result.items()))
+for key, value in sortedResults.items():
+    print("%s %i" % (key, value))
